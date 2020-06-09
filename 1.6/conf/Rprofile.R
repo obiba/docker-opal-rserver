52298db91df8b6885ac83472b9bce5f38255fb03
Yannick Marcon <yannick.marcon@obiba.org>
2020-06-09 13:57:51 +0200

    branched layout

--- 1.6/conf/Rprofile.R
+++
@@ -1,8 +0,0 @@
-# install R libraries in a R version independent directory
-if (!file.exists("/srv/R/library")) {
-  dir.create("/srv/R/library", recursive=TRUE)
-}
-.libPaths("/srv/R/library")
-# for Pandoc
-Sys.setenv(HOME="/srv")
-# newline required
